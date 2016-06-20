3c8c740f5e8a90ef7918c483717168a7a9d50d9c
sohiniu <sohini.upadhyay@gmail.com>
2016-06-20 17:14:01 -0500

    Update caffe_setup.sh

--- setup_scripts/caffe_setup.sh
+++ setup_scripts/caffe_setup.sh
@@ -2,17 +2,18 @@ export CUDA_VISIBLE_DEVICES=`cat /tmp/pbs.prologue.$PBS_JOBID`
 
 export PATH=/usr/local/Python-2.7.8/bin:$PATH
 export PATH=/usr/local/protobuf/bin:$PATH
-export PATH=/usr/local/caffe.latest/bin:$PATH
+export PATH=/usr/local/caffe/bin:$PATH #or use caffe.latest
 export PATH=/usr/local/cuda/bin:$PATH
 
 export LD_LIBRARY_PATH=/usr/local/gcc-4.9.1/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/gcc-4.9.1/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64/save:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/leveldb-1.18/lib64:$LD_LIBRARY_PATH 
 export LD_LIBRARY_PATH=/usr/local/opencv/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/Python-2.7.8/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/protobuf/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/local/caffe.latest/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/caffe/lib:$LD_LIBRARY_PATH #or use caffe.latest
 export LD_LIBRARY_PATH=/usr/local/hdf5/lib:$LD_LIBRARY_PATH
