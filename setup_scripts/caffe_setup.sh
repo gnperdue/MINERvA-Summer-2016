72ae9d96ccd54d183c2720495699167c8e727b41
sohiniu <sohini.upadhyay@gmail.com>
2016-08-15 13:19:59 -0500

    latest -> gradient-reversal

--- setup_scripts/caffe_setup.sh
+++ setup_scripts/caffe_setup.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=`cat /tmp/pbs.prologue.$PBS_JOBID`
 
 export PATH=/usr/local/Python-2.7.8/bin:$PATH
 export PATH=/usr/local/protobuf/bin:$PATH
-export PATH=/usr/local/caffe.latest/bin:$PATH #or use caffe.latest
+export PATH=/usr/local/caffe.gradient-reversal/bin:$PATH #or use caffe.latest
 export PATH=/usr/local/cuda/bin:$PATH
 
 export LD_LIBRARY_PATH=/usr/local/gcc-4.9.1/lib:$LD_LIBRARY_PATH
@@ -15,5 +15,5 @@ export LD_LIBRARY_PATH=/usr/local/leveldb-1.18/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/opencv/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/Python-2.7.8/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/protobuf/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/local/caffe.latest/lib:$LD_LIBRARY_PATH #or use caffe.latest
+export LD_LIBRARY_PATH=/usr/local/caffe.gradient-reversal/lib:$LD_LIBRARY_PATH #or use caffe.latest
 export LD_LIBRARY_PATH=/usr/local/hdf5/lib:$LD_LIBRARY_PATH
