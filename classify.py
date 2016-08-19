1e16d35d2c9277c1b37aa5e1f559021ea2899706
sohiniu <sohini.upadhyay@gmail.com>
2016-08-19 16:37:57 -0500

    added debug feature

--- classify.py
+++ classify.py
@@ -7,19 +7,24 @@ import caffe
 
 # Example usage:
 # python classify.py <path to prototxt> <path to model> <test iter> \
-#   <test batch size> <output file>
+#   <test batch size> <label:n-hadmultmeas/segments> <output file>
 
 # Run parameters
-if len(sys.argv) == 6:
+if len(sys.argv) == 7:
     prototxt = sys.argv[1]
     model = sys.argv[2]
     iter = int(sys.argv[3])
     batch_size = int(sys.argv[4])
-    outfile = sys.argv[5]
+    label = sys.argv[5]
+    outfile = sys.argv[6]
 else:
     sys.exit("Incorrect number of arguments")
-
-
+    
+if label=="segments":
+    confusionMatrix = np.zeros([11,11])
+else:
+    confusionMatrix = np.zeros([6,6]) #change to 6x6 for hadmult
+    
 # Initialize Network
 caffe.set_mode_cpu()
 # Import net
@@ -29,14 +34,16 @@ with open(outfile, "w") as f:
     for i in xrange(iter):
         net.forward()
         predictions = net.blobs['output'].data.argmax(1)
+        actuals = net.blobs[label].data
         predvecs = net.blobs['output'].data
         eventids = net.blobs['eventids'].data
         for j in xrange(batch_size):
+            actual = actuals[j]
             prediction = predictions[j]
             predvec = predvecs[j]
             eventid = eventids[j]
             f.write(str(eventid) + "\t" + str(prediction) +
-                    "\t" + str(predvec)+"\n")
+                    "\t" + str(actual) + "\t" str(predvec)+"\n")
 f.close()
 
 
